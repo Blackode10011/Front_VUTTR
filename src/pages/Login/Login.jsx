cc95c4f814c4f557fa20825877c1b8a634506ace
Shumax <maxcastro.mv@gmail.com>
2020-10-07 17:43:38 -0300

    Form SignUp

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -60,7 +60,6 @@ function Login () {
 					Sign Up
 				</Link>
 			</form>
-			
 		</div>
 	);
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
 
+import './SignUp.scss';
+
 import Topbar from '../../components/Topbar';
+import useForm from '../../hooks';
 
 function SignUp() {
+
+	const initValues = {
+		name: '',
+		email: '',
+		password: '',
+	}
+
+	const { values, handleChange } = useForm(initValues);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+	};
+
 	return (
 		<div className='container'>
 			<Topbar/>
+			<form className='signUp' onSubmit={handleSubmit}>
+				<input
+					type='text'
+					name='name'
+					placeholder='User Name'
+					value={values.name}
+					onChange={handleChange}
+					required
+				/>
+				<input
+					type='email'
+					name='email'
+					placeholder='Email Address'
+					value={values.email}
+					onChange={handleChange}
+					required
+				/>
+				<input
+					type='password'
+					name='password'
+					placeholder='Password'
+					value={values.password}
+					onChange={handleChange} 
+					required
+				/>
+				<button type='submit'>
+					Account Create
+				</button>
+			</form>	
 		</div>
 	);
 };
